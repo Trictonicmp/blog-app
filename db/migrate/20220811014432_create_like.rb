7f286d3838ac39f4469bec268d7f30ecf08f7d9f
Amaury Permer <code_shadow@hotmail.com>
2022-08-10 21:01:36 -0500

    Add schema with rake

--- db/migrate/20220811014432_create_like.rb
+++ db/migrate/20220811014432_create_like.rb
@@ -1,8 +1,8 @@
 class CreateLike < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references author:, null: false, foreign_key: { to_table: 'user'}
-      t.references post: null:false, foreign_key: { to_table: 'posts' }
+      t.references :author, null: false, foreign_key: { to_table: 'users'}
+      t.references :post, null:false, foreign_key: { to_table: 'posts' }
 
       t.timestamps
     end
