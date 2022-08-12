cd7382955bf865fe578a6241c9ec925dbcb5df24
Trictonicmp <code_shadow@hotmail.com>
2022-08-11 23:56:52 -0500

    Add Comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  blongs_to :post, class_name: 'Post'
+
+  def add_comment_count
+    post.increment!(:comments_counter)
+  end
+
+  after_save :add_comment_count
+end
