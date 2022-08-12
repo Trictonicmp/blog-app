0c443143d223c2d8b9aa9280fdd60c330463b855
Trictonicmp <code_shadow@hotmail.com>
2022-08-11 23:52:30 -0500

    Add three most recent post and validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-  has_many :comments
-  has_many :likes
-  has_many :posts
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id'
+
+  def three_most_recent_post
+    posts.order(created_at: :desc).first(3)
+  end
+
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :name, presence: true, length: { in: 2...25 }
 end
