93dffd4528041d9fc43dc39efcab758935ee976f
Trictonicmp <code_shadow@hotmail.com>
2022-08-17 23:50:16 -0500

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  def index 
+  def index
     @user = User.find(params[:user_id])
     @posts = Post.all.where(author: @user).order(created_at: :desc)
   end
