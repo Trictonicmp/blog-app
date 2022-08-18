40cbccb31710019012e4a51c62c78a070fd51765
Trictonicmp <code_shadow@hotmail.com>
2022-08-17 21:46:13 -0500

    Add index implementation for post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index 
+    @user = User.find(params[:user_id])
+    @posts = Post.all.where(author_id: params[:id])
+  end
 
   def show; end
 end
