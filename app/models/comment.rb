a872e5c1bd2d0f9363e740e76f6bef8d629c40e2
Trictonicmp <code_shadow@hotmail.com>
2022-08-19 17:01:40 -0500

    Fix issue related to post counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :add_comment_count
+  after_create :add_comment_count
 
   def add_comment_count
     post.increment!(:comments_counter)
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :add_likes_count
+  after_create :add_likes_count
 
   def add_likes_count
     post.increment!(:likes_counter)
