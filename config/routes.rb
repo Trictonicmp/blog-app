be636ea344a2199cea559c8e3b94b8c35f197a5e
Trictonicmp <code_shadow@hotmail.com>
2022-08-19 13:02:45 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
-      resources :comments, only: [:add, :new]
-      resources :likes, only: [:add, :remove]
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:new, :destroy]
     end
   end
+  
+  resources :posts, only: [:new, :create]
   # get'/users/:id', to: 'users#show'
   # get'/users/:id/posts', to: 'posts#index'
   # get'users/:id/posts/:post_id', to: 'posts#show'
