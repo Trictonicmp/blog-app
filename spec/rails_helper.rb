477e87259a44220558a7535e968ea2862d37e06a
Trictonicmp <code_shadow@hotmail.com>
2022-08-23 11:38:59 -0500

    Add setup for capybara and selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,10 @@ RSpec.configure do |config|
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
-
+  # Capybara.register_driver :selenium_chrome do |app|
+  #  Capybara::selenium::Driver.new(app, browser: :firefox)
+  # end
+  # Capybara.default_driver = :selenium_firefox
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
@@ -62,6 +65,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  
+  Capybara.configure do |config|
+    config.run_server = false
+  end
+  session = Capybara::Session.new(:selenium)
+
   config.before(:each) do
     DatabaseCleaner.clean_with(:truncation)
   end
