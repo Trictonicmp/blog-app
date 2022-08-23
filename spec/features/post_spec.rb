bbf264fd8b3ebf86b1ab0e53480e48f9a3363f67
Trictonicmp <code_shadow@hotmail.com>
2022-08-23 17:59:27 -0500

    Fix tests due to seed

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,48 +2,40 @@ require 'rails_helper'
 
 RSpec.describe 'Post', type: :system do
   it 'shows the post title' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('My forth post')
+    visit('http://localhost:3000/users/1/posts/6')
+    expect(page).to have_content('My first post')
   end
 
   it 'displays who wrote the post' do
-    visit('http://localhost:3000/users/136/posts/140')
+    visit('http://localhost:3000/users/1/posts/6')
     expect(page).to have_content('Lilly')
   end
 
   it 'shows how many comments the post has' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('Comments: 11')
+    visit('http://localhost:3000/users/1/posts/6')
+    expect(page).to have_content('Comments: 3')
   end
 
   it 'shows how many likes the post has' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('Likes: 2')
+    visit('http://localhost:3000/users/1/posts/6')
+    expect(page).to have_content('Likes: 3')
   end
 
   it 'shows posts body' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('Lorem ipsum')
+    visit('http://localhost:3000/users/1/posts/6')
+    expect(page).to have_content('This is my first post')
   end
 
   it 'shows the names of each commentor' do
-    visit('http://localhost:3000/users/136/posts/140')
+    visit('http://localhost:3000/users/1/posts/6')
     expect(page).to have_content('Tom')
     expect(page).to have_content('Josh')
   end
 
   it 'shows the comments the post has' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('this is my sixth comment six')
-    expect(page).to have_content('this is my fifth comment five')
-    expect(page).to have_content('this is my fourth comment four')
-    expect(page).to have_content('this is my third comment three')
-    expect(page).to have_content('this is my second comment two')
-    expect(page).to have_content('this is my first comment one')
-    expect(page).to have_content('nice post')
-    expect(page).to have_content('other comment')
-    expect(page).to have_content('ab')
-    expect(page).to have_content('dd')
-    expect(page).to have_content('Hey you!')
+    visit('http://localhost:3000/users/1/posts/6')
+    expect(page).to have_content('Hi Lilly!')
+    expect(page).to have_content('Nice to see you here!')
+    expect(page).to have_content('Liked!!')
   end
 end
