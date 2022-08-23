1aa1ff2b41ceabe05d23aea4437697dda461b24b
Trictonicmp <code_shadow@hotmail.com>
2022-08-23 12:38:59 -0500

    Add integration tests for homepage

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,9 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe 'Homepage', type: :system do  
+RSpec.describe 'Homepage', type: :system do
+  
   it 'shows the names of the users' do
     visit('http://localhost:3000/')
     expect(page).to have_content('Lilly') 
-    expect(page).to have_content('Josh') 
+    expect(page).to have_content('Josh')
+    expect(page).to have_content('Tom')
+  end
+  
+  it 'displays image of each user' do
+    visit('http://localhost:3000/')
+    expect(page).to have_xpath("//img[@alt='Lilly']")
+    expect(page).to have_xpath("//img[@alt='Josh']")
+    expect(page).to have_xpath("//img[@alt='Tom']")
+  end
+  
+  it 'shows numbers of posts of each user' do
+    visit('http://localhost:3000/')
+    expect(page).to have_content("Number of posts: 0")
+    expect(page).to have_content("Number of posts: 4")
+    expect(page).to have_content("Number of posts: 9")
+  end
+  
+  it 'redirects to the user/show page' do
+    visit('http://localhost:3000/')
+    #click_link('Lilly')
+    #expect(page).to have_current_path(users_path(id: 136))
+    expect(page).to have_link('Lilly', href: '/users/136')
   end
 end
