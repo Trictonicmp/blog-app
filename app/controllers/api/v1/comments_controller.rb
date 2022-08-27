578ec1decb5df1b0a84781a784af7c750db84906
arturo <ahpareja@gmail.com>
2022-08-26 20:25:46 -0500

    Add comment api action create

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::CommentsController < ApplicationController
     @comments = Comment.all.where(post_id: post_id).order(created_at: :desc)
     render json: @comments
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end 
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:text, :post_id, :author_id)
+    end
 end
